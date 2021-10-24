daa933b4015c02d09017c4c03deb88bf9d3b4b94
Lucas de Souza <lucasdsm.personal@gmail.com>
2021-10-24 11:04:42 -0300

    Criando conexao com DB

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+//Conectando no DB
+mongoose.connect(process.env.CONNECT_DB, () => {
+    console.log("Conectado ao DB com sucesso!");
+});
 
 app.get('/', (req,res) => {
     res.send("Bem vindo!");
-})
+});
 
 app.listen(3000, () => console.log('Servidor online!'));
